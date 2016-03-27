70226b14009c81c7ddd0d9b0bf3893de68be1383
ZakharSydorenko <zakhar@MBP-Zakhar.Dlink>
2016-03-28 01:48:47 +0300

    add search

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
+  def self.search(search)
+    # Title is for the above case, the OP incorrectly had 'name'
+    where("title LIKE ?", "%#{search}%")
+  end
 end
